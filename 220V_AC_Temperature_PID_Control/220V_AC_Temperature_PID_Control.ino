34638a9680db4c8a8981c49d168ab8f4900c8228
timmtonga <timmtonga@gmail.com>
2020-04-27 11:32:21 +0200

    Resolved conflicts

--- 220V_AC_Temperature_PID_Control/220V_AC_Temperature_PID_Control.ino
+++ 220V_AC_Temperature_PID_Control/220V_AC_Temperature_PID_Control.ino
@@ -3,19 +3,10 @@
 #include <Wire.h> 
 
 //Inputs and outputs
-<<<<<<< HEAD
+
 int firing_pin = 32;
 int zero_cross = 33;
-//int button_pin = 5;
-=======
-int firing_pin = 4;
-//int increase_pin = 11;
-//int decrease_pin = 12;
-int zero_cross = 8;
-//int thermoDO = 9;
-//int thermoCS = 10;
-//int thermoCLK = 13;
->>>>>>> 4b2920f08f34d1650ef573ef916cba5eee25b9ee
+
 
 LiquidCrystal_I2C lcd(0x27,20,4);  //sometimes the adress is not 0x27. Change to 0x3f if it dosn't work.
 Weather sensor;
@@ -23,14 +14,9 @@ Weather sensor;
 //Variables
 int last_CH1_state = 0;
 bool zero_cross_detected = false;
-<<<<<<< HEAD
-int maximum_firing_delay = 7400;
-int firing_delay = maximum_firing_delay;
-=======
 int firing_delay = 9000;
-//////////////////////////////////////////////////////
 int maximum_firing_delay = 9000;
->>>>>>> 4b2920f08f34d1650ef573ef916cba5eee25b9ee
+
 /*Later in the code you will se that the maximum delay after the zero detection
  * is 7400. Why? Well, we know that the 220V AC voltage has a frequency of around 50-60HZ so
  * the period is between 20ms and 16ms, depending on the country. We control the firing
@@ -39,17 +25,13 @@ int maximum_firing_delay = 9000;
 
 unsigned long previousMillis = 0; 
 unsigned long currentMillis = 0;
-<<<<<<< HEAD
-int temp_read_Delay = 500;
-int real_temperature = 0;
-int setpoint = 30;
-=======
+
 int temp_read_Delay = 1000;
 float real_temperature = 0;
 int setpoint = 45;
 //bool pressed_1 = false;
 //bool pressed_2 = false;
->>>>>>> 4b2920f08f34d1650ef573ef916cba5eee25b9ee
+
 
 //PID variables
 float PID_error = 0;
@@ -60,13 +42,9 @@ int PID_value = 0;
 int kp = 203;   int ki= 7.2;   int kd = 1.04;
 int PID_p = 0;    int PID_i = 0;    int PID_d = 0;
 
-<<<<<<< HEAD
-void setup() 
-{
-=======
+
 void setup() {
   //Define the pinsmaximum_firing_delay - PID_value
->>>>>>> 4b2920f08f34d1650ef573ef916cba5eee25b9ee
   pinMode (firing_pin,OUTPUT); 
   pinMode (zero_cross,INPUT_PULLUP);  
   attachInterrupt(digitalPinToInterrupt(zero_cross), zero_crossing, CHANGE);
@@ -77,31 +55,15 @@ void setup() {
   Serial.begin(9600);
 }
 
-<<<<<<< HEAD
-void loop() 
-{   
-=======
 
 void loop() {   
   
->>>>>>> 4b2920f08f34d1650ef573ef916cba5eee25b9ee
   currentMillis = millis();           //Save the value of time before the loop
    /*  We create this if so we will read the temperature and change values each "temp_read_Delay"
     *  value. Change that value above iv you want. The MAX6675 read is slow. Tha will affect the
     *  PID control. I've tried reading the temp each 100ms but it didn't work. With 500ms worked ok.*/
   if(currentMillis - previousMillis >= temp_read_Delay){
     previousMillis += temp_read_Delay;              //Increase the previous time for next loop
-<<<<<<< HEAD
-    real_temperature = (getTemperature(),0); //get the real temperature in Celsius degrees
-    PID_error = setpoint - real_temperature; //Calculate the pid ERROR
-    if(PID_error > 30) //integral constant will only affect errors below 30ºC             
-      PID_i = 0;
-       
-    PID_p = kp * PID_error; //Calculate the P value
-    PID_i = PID_i + (ki * PID_error); //Calculate the I value
-    timePrev = Time; // the previous time is stored before the actual time read
-    Time = millis(); // actual time read
-=======
     real_temperature = (getTemperature());  //get the real temperature in Celsius degrees
     Serial.println(getTemperature());
 
@@ -115,21 +77,18 @@ void loop() {
     PID_i = PID_i + (ki * PID_error);               //Calculate the I value
     timePrev = Time;                    // the previous time is stored before the actual time read
     Time = millis();                    // actual time read
->>>>>>> 4b2920f08f34d1650ef573ef916cba5eee25b9ee
+
     elapsedTime = (Time - timePrev) / 1000;   
     PID_d = kd*((PID_error - previous_error)/elapsedTime); //Calculate the D value
     PID_value = PID_p + PID_i + PID_d; //Calculate total PID value
     //We define firing delay range between 0 and 7400. Read above why 7400!!!!!!!
-    if(PID_value < 0)
-<<<<<<< HEAD
-      PID_value = 0;
-    if(PID_value > 7400)
-      PID_value = 7400;
-=======
-    {      PID_value = 0;       }
-    if(PID_value > 9000)
-    {      PID_value = 9000;    }
->>>>>>> 4b2920f08f34d1650ef573ef916cba5eee25b9ee
+    if(PID_value < 0){      
+      PID_value = 0;       
+    }
+    if(PID_value > 9000){      
+      PID_value = 9000;    
+    }
+
     //Printe the values on the LCD
     lcd.clear();
     lcd.setCursor(0,0);
@@ -150,16 +109,6 @@ void loop() {
 //  }
 
   //If the zero cross interruption was detected we create the 100us firing pulse  
-<<<<<<< HEAD
-  if(zero_cross_detected){
-    delayMicroseconds(maximum_firing_delay - PID_value); //This delay controls the power
-    digitalWrite(firing_pin,HIGH);
-    delayMicroseconds(100);
-    digitalWrite(firing_pin,LOW);
-    zero_cross_detected = false;
-  } 
-}
-=======
   if (zero_cross_detected)     
     {
       delayMicroseconds(maximum_firing_delay - PID_value); //This delay controls the power
@@ -224,19 +173,15 @@ ISR(PCINT0_vect){
     }*/
 }
 //End of interruption vector for pins on port B: D8-D13
->>>>>>> 4b2920f08f34d1650ef573ef916cba5eee25b9ee
+
 
 //Extracts temperature from the Sensor
 float getTemperature()
 {
   //Measure Temperature from Si7021
-<<<<<<< HEAD
   int tempC = sensor.getTemp();
   Serial.println(tempC,0);
-=======
-  float tempC = sensor.getTemp();
-  //Serial.println(tempC,0);
->>>>>>> 4b2920f08f34d1650ef573ef916cba5eee25b9ee
+
   return tempC;
 }
 
