86a68de0630cbdde70fc05fcc10f39587d495cdd
Duntar <mwabilungu@gmail.com>
2020-05-15 20:10:18 +0200

    PID control code for the PCB

---
+++ 220V_AC_Temperature_PID_Control PCB code/220V_AC_Temperature_PID_Control.ino
@@ -0,0 +1,126 @@
+#include <LiquidCrystal_I2C.h>
+#include <Wire.h> 
+
+//Inputs and outputs
+int firing_pin = 33;
+int zero_cross = 25;
+const int ADDR = 0x40;
+const int MeasureTemp = 0xE3;
+int X0,X1,temp;
+double X,X_out;
+
+LiquidCrystal_I2C lcd(0x27,20,4);  //sometimes the adress is not 0x27. Change to 0x3f if it dosn't work.
+
+//Variables
+int last_CH1_state = 0;
+bool zero_cross_detected = false;
+const int maximum_firing_delay = 9000;
+// Max firing delay set to 9ms based on AC frequency of 50Hz
+unsigned long previousMillis = 0; 
+unsigned long currentMillis = 0;
+int temp_read_Delay = 500;
+float real_temperature = 0;
+int setpoint = 75;
+int print_firing_delay;
+//PID variables
+float PID_error = 0;
+float previous_error = 0;
+float elapsedTime, Time, timePrev;
+int PID_value = 0;
+//PID constants
+int kp = 1800;   int ki= 1.2;   int kd = 30000;
+int PID_p = 0;    int PID_i = 0;    int PID_d = 0;
+
+//Zero Crossing Interrupt Function
+void IRAM_ATTR zero_crossing()
+{
+  //If the last state was 0, then we have a state change...
+  if(last_CH1_state == 0) 
+    zero_cross_detected = true; //We have detected a state change! We need both falling and rising edges
+  //If pin 8 is LOW and the last state was HIGH then we have a state change      
+  else if(last_CH1_state == 1){ 
+    zero_cross_detected = true;    //We have detected a state change!  We need both falling and rising edges.
+    last_CH1_state = 0;            //Store the current state into the last state for the next loop
+  }   
+}
+
+void setup() {
+  //Define the pinsmaximum_firing_delay - PID_value
+  pinMode (firing_pin,OUTPUT); 
+  pinMode (zero_cross,INPUT_PULLUP);  
+  attachInterrupt(digitalPinToInterrupt(zero_cross), zero_crossing, CHANGE);
+  lcd.begin(); //Begin the LCD communication through I2C
+  lcd.backlight();  //Turn on backlight for LCD
+  //inititalize the I2C the sensor and bing it
+  Serial.begin(9600);
+}
+
+void loop() 
+{   
+  currentMillis = millis();           //Save the value of time before the loop
+  // We create this if so we will read the temperature and change values each "temp_read_Delay"
+  if(currentMillis - previousMillis >= temp_read_Delay){
+    previousMillis += temp_read_Delay;              //Increase the previous time for next loop
+    real_temperature = (GetTemp(17,16));  //get PID Control Temperature
+    Serial.print("," + String ((maximum_firing_delay - PID_value)/100.0));
+    Serial.print("," + String(real_temperature)); 
+    Serial.print("," +String(GetTemp(18,19))); // Lower Chamber
+    Serial.println();      
+    PID_error = setpoint - real_temperature;        //Calculate the pid ERROR
+    if(PID_error > 30)                              //integral constant will only affect errors below 30ºC             
+      PID_i = 0;
+    PID_p = kp * PID_error;                         //Calculate the P value
+    PID_i = PID_i + (ki * PID_error);               //Calculate the I value
+    timePrev = Time;                    // the previous time is stored before the actual time read
+    Time = millis();                    // actual time read
+    elapsedTime = (Time - timePrev) / 1000;   
+    PID_d = kd*((PID_error - previous_error)/elapsedTime); //Calculate the D value
+    PID_value = PID_p + PID_i + PID_d; //Calculate total PID value
+    //We define firing delay range between 0 and 7400. Read above why 7400!!!!!!!
+    if(PID_value < 0)      
+      PID_value = 0;       
+    if(PID_value > maximum_firing_delay)      
+      PID_value = maximum_firing_delay;    
+    //Print the values on the LCD
+    Wire.begin(21,22,50000);
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("Set: ");
+    lcd.setCursor(5,0);
+    lcd.print(setpoint);
+    lcd.setCursor(0,1);
+    lcd.print("Real temp: ");
+    lcd.setCursor(11,1);
+    lcd.print(real_temperature);
+    previous_error = PID_error; //Remember to store the previous error.
+  }
+
+  //If the zero cross interruption was detected we create the 100us firing pulse  
+  if (zero_cross_detected){
+      delayMicroseconds(maximum_firing_delay - PID_value); //This delay controls the power
+      digitalWrite(firing_pin,HIGH);
+      delayMicroseconds(100);
+      digitalWrite(firing_pin,LOW);
+      zero_cross_detected = false;
+  } 
+}
+//End of void loop
+
+//Extracts temperature from the Si7021 Sensor
+double GetTemp(int SDA_Pin, int SLC_pin) {
+  Wire.begin(SDA_Pin,SLC_pin,5000);
+  Wire.beginTransmission(ADDR);
+  Wire.write(MeasureTemp);
+  Wire.endTransmission();
+  Wire.requestFrom(ADDR,2);
+  if(Wire.available()<=2);{
+    X0 = Wire.read();
+    X1 = Wire.read();
+    X0 = X0<<8;
+    X_out = X0+X1;
+  }
+  /**Calculate temperature**/
+  X=(175.72*X_out)/65536;
+  X=X-46.85;
+  return X;
+}
